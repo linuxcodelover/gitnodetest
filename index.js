661c355f206d94bbc256a71b9668abb2f83700c5
Nikul Thakor <112260071+linuxcodelover@users.noreply.github.com>
2024-09-12 18:24:15 +0530

    Update index.js
    updated at the remote side

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World from the test branch!');
+  res.send('Hello World from the test branch and does the remote changes!');
 });
 
 app.listen(port, () => {
